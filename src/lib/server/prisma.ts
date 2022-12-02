5bc10a7f685401aca05b5d741078d4f335f102db
Hunter Johnston <johnstonhuntera@gmail.com>
2022-12-01 22:35:56 -0500

    prisma slow

--- src/lib/server/prisma.ts
+++ src/lib/server/prisma.ts
@@ -1,32 +1,36 @@
 import { PrismaClient } from '@prisma/client';
 import { CONNECTION_POOL_URL } from '$env/static/private';
 
-let prisma: PrismaClient;
-
-declare global {
-	var __db__: PrismaClient;
-}
-
-if (process.env.NODE_ENV === 'production') {
-	prisma = new PrismaClient({
-		datasources: {
-			db: {
-				url: CONNECTION_POOL_URL,
-			},
+export const prisma = new PrismaClient({
+	datasources: {
+		db: {
+			url: CONNECTION_POOL_URL,
 		},
-	});
-} else {
-	if (!global.__db__) {
-		global.__db__ = new PrismaClient({
-			datasources: {
-				db: {
-					url: CONNECTION_POOL_URL,
-				},
-			},
-		});
-	}
-	prisma = global.__db__;
-	prisma.$connect();
-}
+	},
+});
 
-export { prisma };
+// declare global {
+// 	var __db__: PrismaClient;
+// }
+
+// if (process.env.NODE_ENV === 'production') {
+// 	prisma = new PrismaClient({
+// 		datasources: {
+// 			db: {
+// 				url: CONNECTION_POOL_URL,
+// 			},
+// 		},
+// 	});
+// } else {
+// 	if (!global.__db__) {
+// 		global.__db__ = new PrismaClient({
+// 			datasources: {
+// 				db: {
+// 					url: CONNECTION_POOL_URL,
+// 				},
+// 			},
+// 		});
+// 	}
+// 	prisma = global.__db__;
+// 	prisma.$connect();
+// }
--- src/routes/jobs/+page.server.ts
+++ src/routes/jobs/+page.server.ts
@@ -1,12 +1,21 @@
 import { prisma } from '$lib/server/prisma';
+import { PrismaClient } from '@prisma/client';
+import { CONNECTION_POOL_URL } from '$env/static/private';
 import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad = async () => {
+export const load: PageServerLoad = async ({ locals }) => {
 	const getJobs = async () => {
-		return await prisma.job.findMany();
+		const jobs = await prisma.job.findMany();
+		return jobs;
 	};
 
 	return {
 		jobs: getJobs(),
 	};
+
+	// let { data: jobs, error } = await locals.sb.from('jobs').select('*');
+
+	// return {
+	// 	jobs,
+	// };
 };
